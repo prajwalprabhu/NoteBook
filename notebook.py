33a94f95872a92617177942421b8124e7ef1e084
T Prajwal Prabhu <66326051+prajwalprabhu@users.noreply.github.com>
2020-08-15 14:00:29 +0530

    Add files via upload

--- notebook.py
+++ notebook.py
@@ -1,18 +1,17 @@
+import os
 import tkinter as tk
-from tkinter import filedialog
-import pickle
+from tkinter import EXCEPTION, filedialog,messagebox
+from json import load,dump
+from threading import Thread, active_count, current_thread
+from time import sleep
 class notebook(object):
+	
 	"""docstring for notebook"""
 	def __init__(self):
-		self.file_opened=0
+		self.file_opened=False
+		self.app=True
 		self.root=tk.Tk()
-		try:
-			with open("Notebook.cache","rb") as f:
-				self.openedfile=pickle.load(f)
-				self.explorer_open=pickle.load(f)
-		except:
-			self.openedfile=None
-			self.explorer_open=False
+		self.load_data()
 		if self.explorer_open:
 			if self.openedfile !=None:
 				self.explorer()
@@ -25,6 +24,7 @@ class notebook(object):
 		self.root.bind("<Control-S>",self.save_as_file)
 		self.root.bind("<Control-o>",self.open_file)
 		self.root.bind("<Control-e>",self.explorer)
+		self.root.bind("<Control-n>",self.new_file)
 		File=tk.Menu(menu)
 		View=tk.Menu(menu)
 		File.add_command(label="New")
@@ -35,62 +35,190 @@ class notebook(object):
 		menu.add_cascade(label="File",menu=File)
 		menu.add_cascade(label="View",menu=View)
 		self.text=tk.Text(self.root)
-		self.text.pack(side="right")
-		if self.openedfile !=None:
-			with open(self.openedfile) as f:
-				self.root.title(self.openedfile)
-				self.text.insert(1.0,f.read())
-
+		self.text.grid(row=0,column=2)
+		self.text.configure(height=42)
+		if self.openedfile !=None and len(self.openedfile)>4:
+			self.open_file(file=self.openedfile)
+		self.thread=Thread(target=self.collect_data)
+		self.thread.start()
 		self.root.mainloop()
-		with open("Notebook.cache","wb") as f:
-			pickle.dump(self.openedfile,f)
-			pickle.dump(not self.explorer_open,f)
+		self.app=False
+		self.check()
+		self.dump_data()
+		self.ask_exit()
 
+		
 
-	def open_file(self,event=None,openedfile=None):
-		self.openedfile=filedialog.askopenfilename(initialdir="/")
-		if len(self.openedfile)>1:
+	def open_file(self,event=None,file=None):
+		self.lastopenedfile=self.openedfile
+		if file==None:
+			self.openedfile=filedialog.askopenfilename(initialdir="/")
+		else:
+			self.openedfile=file
+		if os.path.isdir(self.openedfile):
+			self.list_dir(file=self.openedfile)
+			pass
+		elif  os.path.isfile(self.openedfile):
 			with open(self.openedfile) as f:
 				self.data=f.read()
 			self.text.delete(1.0,tk.END)
 			self.text.insert(1.0,self.data)
 			self.root.title(self.openedfile)
-			self.file_opened=1
+			self.file_opened=True
+			if not self.explorer_open:
+				self.explorer()
+				self.explorer()
+		else:
+			messagebox.showerror("No File Opened","No File is Selected to open")
 	def save_as_file(self,event=None):
 		a=filedialog.asksaveasfile()
 		if a !=None:
-			self.data=self.text.get(1.0,tk.END)
-			a.write(self.data)
+			a.write(self.page_data)
 			a.close()
-			self.root.title(a.name)
+			if self.app:
+				self.root.title(a.name)
 			self.openedfile=a.name
 	def save_file(self,event=None):
-		if self.file_opened:
+		if os.path.isfile(self.openedfile):
 			with open(self.openedfile,"w") as f:
-				f.write(self.text.get(1.0,tk.END))
+				f.write(self.page_data)
 				self.root.title(self.openedfile)
 		else:
 			self.save_as_file()
 
 	def explorer(self,event=None):
-		if self.explorer_open:
-
-			self.View_frame=tk.PanedWindow()
-			self.View_frame.pack(side="left",padx=10,fill="y",expand=1,anchor='nw')
-			from os import listdir
-			openedfile=self.openedfile.replace("/","\\")
-			openedfile=openedfile.split("\\")
-			openedfile.pop(-1)
-			opendir=openedfile
-			directory=''
-			for i in opendir:
-				directory=directory+i+"\\"
-			for i,file in enumerate(listdir(directory)):
-				if not ".mp4" in file:
-					self.l=tk.Label(self.View_frame,text=file)
-					self.l.grid(row=i,column=0)
+		if self.explorer_open and len(self.openedfile)>=4:
+			sframe=tk.Frame(self.root)
+			sframe.grid(row=0,column=1)
+			sb=tk.Scrollbar(sframe,orient="vertical")
+			sb.pack(fill="y")
+			self.list=tk.Listbox(self.root,yscrollcommand=sb.set)
+			self.list.grid(row=0,column=0)
+			self.list.configure(height=43)
+			sb.config(command=self.list.yview)
+			self.list_dir()
+			
 		if not self.explorer_open:
-			self.View_frame.destroy()
+			self.list.destroy()
 		self.explorer_open=not self.explorer_open
 
+	def new_file(self,event=None):
+
+		if len(self.openedfile)>5:
+			if os.path.isfile(self.openedfile):
+				with open(self.openedfile) as f:
+					data=f.read()
+				text=self.page_data
+				self.asddir,name=os.path.split(self.openedfile)
+				if not text==data:
+					choice=messagebox.askyesno("File not saved","Doyou want to save the file")
+					if choice:
+						self.save_file()
+		else:
+			self.asddir=os.getcwd()
+		self.root.title(os.path.normcase(f"{self.asddir}//Untitled-1.txt"))
+		self.lastopenedfile=self.openedfile
+		self.openedfile=os.path.normcase(os.path.join(self.asddir,"Untitled-1.txt"))
+		try:
+			self.text.delete(1.0,tk.END)
+		except AttributeError:
+			pass
+	
+	def check(self):
+		if not os.path.isfile(self.openedfile):
+			choice=messagebox.askyesno("File not saved","Doyou want to save the file")
+			if choice:
+				self.save_file()
+				print("if")
+			else:
+				self.openedfile=self.lastopenedfile
+				print("else")
+	def dump_data(self):
+		try:
+			with open(os.getcwd()+"\\data\\Note_Book_data.json","w") as f:
+				self.json_data["path"]=self.openedfile
+				self.json_data["explorer"]=not self.explorer_open
+				dump(self.json_data,f,indent=4,sort_keys=True)
+		except AttributeError:
+			pass
+	def collect_data(self):
+		print("collecting data")
+		try:
+			while 1:
+				if self.app==True:
+					self.page_data=self.text.get(1.0,tk.END)
+				else:
+					break
+		except Exception as e:
+			return None
+						
+	def load_data(self):
+		try:
+			with open(os.getcwd()+"\\data\\Note_Book_data.json","r") as f:
+				self.json_data=load(f)
+				self.openedfile=self.json_data["path"]
+				self.explorer_open=self.json_data["explorer"]
+		except Exception as e:
+			self.openedfile=None
+			self.explorer_open=False
+	def ask_exit(self):
+		a=messagebox.askyesno("Exit","Do You Wnat to exit")
+		if not a:
+			notebook()
+	def double_click(self,event=None):
+		file=self.list.get("anchor")
+		file_temp=list(file)
+		if "folder" in file:
+			print("folder found")
+			for _ in range(0,9):
+				file_temp.pop(0)
+			file=''
+			for i in range(0,len(file_temp)):
+				file=file+file_temp[i]
+				pass
+
+		
+		if os.path.isfile(self.openedfile):
+			dir,_=os.path.split(self.openedfile)
+		else:
+			dir=self.openedfile
+		file_temp=os.path.normcase(os.path.join(dir,file))
+		print(file_temp)
+		self.open_file(file=file_temp)
+
+	def list_dir(self,file=None):
+		if file==None:
+			dir,file = os.path.split(self.openedfile)
+		else:
+			dir=file
+		self.list.delete(0,tk.END)
+		for i,file in enumerate(os.listdir(dir)):
+			if not ".mp4" in file:
+				
+
+				if  os.path.isdir(os.path.join(dir,file)):
+					self.list.insert("end",f"folder \t {file}")
+				else:
+					# print("else encontered")
+					self.list.insert("end",file)
+		self.list.bind("<Button-1>",self.double_click)
+	def sleepp(self,t):
+		sleep(t)
+		notebook()
+
 notebook()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
